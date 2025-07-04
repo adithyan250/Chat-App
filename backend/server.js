9976ccfa7c2ac4e9dd844177b3bb7e3c026441b7
Adithyan PB <adithyanpb2005@gmail.com>
2025-07-04 21:25:52 +0530

    production ready

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,10 +9,11 @@ import connctToMongoDb from "./db/connectToMongoDB.js";
 import userRoutes from "./routes/user.route.js"
 import { app, server } from "./socket/socket.js";
 
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve(); 
+
 dotenv.config();
-
-
-const PORT = process.env.PORT || 5000
 
 app.use(express.json()); 
 app.use(cookieParser());
@@ -20,10 +22,16 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res)=> {
-    res.send("Hello, World!");
-})
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
+
+// app.get("/:wildcard(*)", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// });
+
+// app.get("*",(req, res)=>{
+//     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// })
 
 
 server.listen(PORT, () =>{
